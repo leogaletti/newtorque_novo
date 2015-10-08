b9687d25bc09e0966423075a18e0ace6acdc1431
Leonardo Galetti <leo.galetti@gmail.com>
2015-10-08 02:22:33 -0300

    Alteração do aquivo index.php para o novo layout do site.

--- site/index.php
+++ site/index.php
@@ -1,38 +1,29 @@
 <?
-	//include "conectar.php";
+    //include "conectar.php";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"[]>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="pt-BR" xml:lang="pt-BR">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title> New Torque - Excelência em Torque </title>
-<meta name="author" content="www.msideias.com.br">
-<meta name="classification" content="industries">
-<META NAME="Keywords" Content="new torque servicos prestacao equipamentos torquimetro valvulas medidores venda">
-<META NAME="Description"  Content="New Torque - Excelência em Torque">
-<META NAME="organization name"  Content="New Torque">
-<meta name="audience" content="ALL">
-<meta name="expires" content="NEVER">
-<meta name="page-type" content="Commercial">
-<meta name="robots" content="INDEX,FOLLOW">
-<meta name="Language" content="Portuguese">
-<meta name="language" content="pt-br">
-<meta name="Revisit" content="2">
-<meta name="revisit-after" content="2 days">
-<meta name="distribution" content="Global">
-<meta name="rating" content="industria">
-<meta name="resource-type" content="document">
-<meta name="doc-class" content="Completed">
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<!--
-<link href="style.css" rel="stylesheet" type="text/css">
-
-<link rel="stylesheet" href="js/coin-slider-styles.css" type="text/css" media="screen" />
--->
-<script type="text/javascript" src="jquery.js"></script>
-<script type="text/javascript" src="script.js"></script>
-<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
-<script type="text/javascript" src="js/coin-slider.min.js"></script>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title> New Torque - Excelência em Torque </title>
+        <meta name="author" content="www.msideias.com.br">
+        <meta name="classification" content="industries">
+        <META NAME="Keywords" Content="new torque servicos prestacao equipamentos torquimetro valvulas medidores venda">
+        <META NAME="Description"  Content="New Torque - Excelência em Torque">
+        <META NAME="organization name"  Content="New Torque">
+        <meta name="audience" content="ALL">
+        <meta name="expires" content="NEVER">
+        <meta name="page-type" content="Commercial">
+        <meta name="robots" content="INDEX,FOLLOW">
+        <meta name="Language" content="Portuguese">
+        <meta name="language" content="pt-br">
+        <meta name="Revisit" content="2">
+        <meta name="revisit-after" content="2 days">
+        <meta name="distribution" content="Global">
+        <meta name="rating" content="industria">
+        <meta name="resource-type" content="document">
+        <meta name="doc-class" content="Completed">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Latest compiled and minified CSS -->
@@ -43,216 +34,97 @@
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <meta charset="utf-8">
-
-</head>
-<body>
-<div id="newt-main">
-    <div class="cleared reset-box"></div>
-    <div class="newt-header">
-        <div class="newt-header-position">
-            <div class="newt-header-wrapper">
-                <div class="cleared reset-box"></div>
-                <div class="newt-header-inner">
-                <div class="newt-headerobject"></div>
-                <script type="text/javascript" src="swfobject.js"></script>
-                <script type="text/javascript">
-                jQuery((function (swf) {
-                    return function () {
-                        swf.switchOffAutoHideShow();
-                        swf.registerObject("newt-flash-object", "9.0.0", "expressInstall.swf");
-                    }
-                })(swfobject));
-                </script>
-                <div id="newt-flash-area">
-                <div id="newt-flash-container">
-                <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="1881" height="470" id="newt-flash-object">
-                    <param name="movie" value="images/flash.swf" />
-                    <param name="quality" value="high" />
-                	<param name="scale" value="default" />
-                	<param name="wmode" value="transparent" />
-                	<param name="flashvars" value="color1=0xFFFFFF&amp;alpha1=.45&amp;framerate1=12&amp;loop=true&amp;wmode=transparent" />
-                    <param name="swfliveconnect" value="true" />
-                    <!--[if !IE]>-->
-                    <object type="application/x-shockwave-flash" data="images/flash.swf" width="1881" height="470">
-                        <param name="quality" value="high" />
-                	    <param name="scale" value="default" />
-                	    <param name="wmode" value="transparent" />
-                	    <param name="flashvars" value="color1=0xFFFFFF&amp;alpha1=.45&amp;framerate1=12&amp;loop=true&amp;wmode=transparent" />
-                        <param name="swfliveconnect" value="true" />
-                    <!--<![endif]-->
-                      	<div class="newt-flash-alt"><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></div>
-                    <!--[if !IE]>-->
-                    </object>
-                    <!--<![endif]-->
-                </object>
-                </div>
-                </div>
-                <div class="newt-logo">
-                                                </div>
-                </div>
+    </head>
+    <body>  
+        <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-collapsed">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">New Torque - Encelência em torque</a>
             </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="menu-collapsed">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Principal</a>
+                    </li>
+                    <li>
+                        <a href="aempresa.php">A Empresa</a>
+                    </li>
+                    <li>
+                        <a href="servicos.php">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="linha-equipamentos.php">Linha Equipamentos</a>
+                    </li>
+                    <li>
+                        <a href="clientes.php">Clientes</a>
+                    </li>
+                    <li>
+                        <a href="videos.php">Vídeos</a>
+                    </li>
+                    <li>
+                        <a href="localizacao.php">Localização</a>
+                    </li>
+                    <li>
+                        <a href="fale-conosco.php">Fale Conosco</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
-        
-    </div>
-    <div class="cleared reset-box"></div>
-<div class="newt-bar newt-nav">
-<div class="newt-nav-outer">
-<div class="newt-nav-wrapper">
-<div class="newt-nav-inner">
-	<div class="newt-nav-center">
-	<ul class="newt-hmenu">
-		<li>
-			<a href="./index.php" class="active">Principal</a>
-		</li>	
-		<li>
-			<a href="./a-empresa.php">A Empresa</a>
-		</li>	
-		<li>
-			<a href="./servicos.php">Servi&ccedil;os</a>
-		</li>	
-		<li>
-			<a href="./linha-equipamentos.php">Linha Equipamentos</a>
-		</li>	
-		<li>
-			<a href="./clientes.php">Clientes</a>
-		</li>	
-		<li>
-			<a href="./videos.php">V&iacute;deos</a>
-		</li>	
-		<li>
-			<a href="./localizacao.php">Localiza&ccedil;&atilde;o</a>
-		</li>	
-		<li>
-			<a href="./fale-conosco.php">Fale Conosco</a>
-		</li>	
-	</ul>
-	</div>
-</div>
-</div>
-</div>
-</div>
-<div class="cleared reset-box"></div>
-<div class="newt-box newt-sheet">
-        <div class="newt-box-body newt-sheet-body">
-            <div class="newt-layout-wrapper">
-                <div class="newt-content-layout">
-                    <div class="newt-content-layout-row">
-                        <div class="newt-layout-cell newt-content">
-<div class="newt-box newt-post">
-    <div class="newt-box-body newt-post-body">
-<div class="newt-post-inner newt-article">
-                                <div class="newt-postcontent">
-<div class="newt-content-layout-wrapper layout-item-0">
-<div class="newt-content-layout layout-item-1">
-    <div class="newt-content-layout-row">
-    <div class="newt-layout-cell layout-item-2" style="width: 75%;">
-        <p style="text-align: center;"><div id="slide" align="center">
-<?
-				$id_query_cli = mysql_query("Select * from destaques where bl_publicar = '1' order by cd_ordem");
-				while($arr = mysql_fetch_assoc($id_query_cli)){
-?>
-					<a href="<? echo($arr[ds_link]) ?>" target="<? echo($arr[ds_target]) ?>" /><img src="fotos_destaque/foto_<? echo($arr[cd_destaque]) ?>.JPG" border="0" /><span class="cs-title"><? echo($arr[ds_legenda]) ?></span></a>
-<?
-				}
-?>
-				</div>
-				<script>
-				$(document).ready(function() {
-				$('#slide').coinslider({ width: 960, height:480 , delay: 5000, links: true, opacity: 0.7 });
-				});
-				</script>
-</p>
-    </div><div class="newt-layout-cell layout-item-2" style="width: 200px;">
-        <p style="text-align: center;"><img width="180" height="482" alt="" src="images/banner_direita_inicio.jpg" /></p>
-    </div>
-    </div>
-</div>
-</div>
-<div class="newt-content-layout-wrapper layout-item-0">
-<div class="newt-content-layout layout-item-1">
-    <div class="newt-content-layout-row">
-    <div class="newt-layout-cell layout-item-2" style="width: 25%;">
-        <p style="text-align: center;"><img width="200" height="134" alt="" src="./imagens/inicial01.jpg" /></p>
-        <p><span style="font-size: 16px; font-weight: bold; color: #000000;">Em &nbsp;destaque:</span></p>
-<p>
-<?
-	$sql = "SELECT * FROM noticias where bl_publicar='1' and bl_destaque='1'";
-	$id_query = mysql_query($sql);
-	$arr = mysql_fetch_assoc($id_query);
-	$titulo=substr($arr[tit_noticia], 0, 100)."...";
-	$codigo=$arr[cd_noticia];
-?>
-    <? echo($titulo) ?><a href="./noticia_completa.php?cd=<? echo($codigo) ?>">Leia mais</a>
-</p>
-    </div>
-    <div class="newt-layout-cell layout-item-2" style="width: 25%;">
-        <p style="text-align: center;"><img width="200" height="134" alt="" src="./imagens/inicial02.jpg" /></p>
-        <p><span style="font-size: 16px; font-weight: bold; color: #000000;">Vantagens da New Torque</span></p>
-        <p>Conhe&ccedil;a nossos principais diferenciais&nbsp;&raquo;&nbsp;<a href="./a-empresa.php">clique aqui</a></p>
-    </div>
-    <div class="newt-layout-cell layout-item-2" style="width: 25%;">
-        <p style="text-align: center;"><img width="200" height="134" alt="" src="./imagens/inicial03.jpg" /></p>
-        <p><span style="font-size: 16px; font-weight: bold; color: #000000;">Agende uma visita</span></p>
-        <p>Deixe seus dados que entraremos em contato&nbsp;&nbsp;&raquo; <a href="./fale-conosco.php">clique aqui</a></p>
-    </div>
-    <div class="newt-layout-cell layout-item-2" style="width: 25%;">
-        <p style="text-align: center;"><img width="200" height="134" alt="" src="./imagens/inicial04.jpg" /></p>
-        <p><span style="font-weight: bold;">Trabalhe conosco</span></p>
-        <p>Envie seu curriculo.<br />
-         » <a href="./trabalhe-conosco.php">clique aqui</a><br />
-        </p>
-    </div>
-    </div>
-</div>
-</div>
+        <!-- /.container -->
+    </nav>
+    <!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-                </div>
-                <div class="cleared"></div>
-                </div>
-
-		<div class="cleared"></div>
-    </div>
-</div>
-
-                          <div class="cleared"></div>
-                        </div>
-                    </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <?
+                $id_query_cli = mysql_query("Select * from destaques where bl_publicar = '1' order by cd_ordem");
+                while($arr = mysql_fetch_assoc($id_query_cli)){
+            ?>
+            <div class="item active">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 1</h2>
                 </div>
             </div>
-            <div class="cleared"></div>
             
-    		<div class="cleared"></div>
-        </div>
-    </div>
-    <div class="newt-footer">
-        <div class="newt-footer-body">
-            <div class="newt-footer-center">
-                <div class="newt-footer-wrapper">
-                    <div class="newt-footer-text">
-                        <a href="#" class="newt-rss-tag-icon" title="RSS"></a>
-                        <table class="newt-article" border="0" cellspacing="0" cellpadding="0" style="margin-right: auto; margin-left: auto; width: 100%;">
-    <tbody>
-        <tr>
-            <td style="text-align: center; width: 90%;" class="">
-                <h3>Rua Cento e Trinta e Um nº 276<br />
-                Bairro Santa Maria -&nbsp;Tim&oacute;teo &nbsp;Cep: 35180-142<br />
-                Email: contato@newtorque.com.br<br /></h3>
-            </td>
-
-            <td style="text-align: center; width: 10%;" class=""><a href="http://www.msideias.com.br" target="_blank" title="Soluções para suas ideias!"><img width="33" height="25" alt="" src="./images/logo_para_sites.png" style="margin-right: 1px; margin-left: 1px; cursor: text;" /></a><a href="adm" target="_blank" title="Acesso ao gerenciador de conteúdo do site"><img width="32" height="32" alt="" src="./images/gerenciador.png" style="margin-right: 1px; margin-left: 1px;" /></a></td>
-        </tr>
-    </tbody>
-</table>
-                        <div class="cleared"></div>
-                        <p class="newt-page-footer"></p>
-                    </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 2</h2>
                 </div>
             </div>
-            <div class="cleared"></div>
+            <div class="item">
+                <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+                <div class="carousel-caption">
+                    <h2>Caption 3</h2>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="cleared"></div>
-</div>
 
-</body>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+    </body>
 </html>
