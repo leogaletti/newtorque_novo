2fab6c637642e1422a04a9c5b525c00bcedd66ee
Leonardo Galetti <leo.galetti@gmail.com>
2015-10-20 00:38:46 -0200

    Pagina de Fotos dos Servicos

--- site/fotos-servicos.php
+++ site/fotos-servicos.php
@@ -3,134 +3,88 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"[]>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="pt-BR" xml:lang="pt-BR">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title> New Torque - Excelência em Torque </title>
-<meta name="author" content="www.msideias.com.br">
-<meta name="classification" content="industries">
-<META NAME="Keywords" Content="new torque servicos prestacao equipamentos torquimetro valvulas medidores venda">
-<META NAME="Description"  Content="New Torque - Excelência em Torque">
-<META NAME="organization name"  Content="New Torque">
-<meta name="audience" content="ALL">
-<meta name="expires" content="NEVER">
-<meta name="page-type" content="Commercial">
-<meta name="robots" content="INDEX,FOLLOW">
-<meta name="Language" content="Portuguese">
-<meta name="language" content="pt-br">
-<meta name="Revisit" content="2">
-<meta name="revisit-after" content="2 days">
-<meta name="distribution" content="Global">
-<meta name="rating" content="industria">
-<meta name="resource-type" content="document">
-<meta name="doc-class" content="Completed">
-<link href="style.css" rel="stylesheet" type="text/css">
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<!-- O arquivo lightbox.css faz parte da biblioteca e é necessária sua inclusão-->
-<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/galeria.css" type="text/css" media="screen" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title> New Torque - Excelência em Torque </title>
+        <meta name="author" content="www.msideias.com.br">
+        <meta name="classification" content="industries">
+        <META NAME="Keywords" Content="new torque servicos prestacao equipamentos torquimetro valvulas medidores venda">
+        <META NAME="Description"  Content="New Torque - Excelência em Torque">
+        <META NAME="organization name"  Content="New Torque">
+        <meta name="audience" content="ALL">
+        <meta name="expires" content="NEVER">
+        <meta name="page-type" content="Commercial">
+        <meta name="robots" content="INDEX,FOLLOW">
+        <meta name="Language" content="Portuguese">
+        <meta name="language" content="pt-br">
+        <meta name="Revisit" content="2">
+        <meta name="revisit-after" content="2 days">
+        <meta name="distribution" content="Global">
+        <meta name="rating" content="industria">
+        <meta name="resource-type" content="document">
+        <meta name="doc-class" content="Completed">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
-<script type="text/javascript" src="jquery.js"></script>
-<script type="text/javascript" src="script.js"></script>
-<!-- incluindo os arquivos da biblioteca LightBox-->
-<script type="text/javascript" src="js/prototype.js"></script>
-<script type="text/javascript" src="js/scriptaculous.js?load=effects"></script>
-<script type="text/javascript" src="js/lightbox.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-<style type="text/css">
-.newt-post .layout-item-0 { margin-bottom: 10px; }
-.newt-post .layout-item-1 { border-spacing: 10px 0px; border-collapse: separate; }
-.newt-post .layout-item-2 { border-top-style:solid;border-right-style:solid;border-bottom-style:solid;border-left-style:solid;border-top-width:1px;border-right-width:1px;border-bottom-width:1px;border-left-width:1px;border-top-color:#3F97A6;border-right-color:#3F97A6;border-bottom-color:#3F97A6;border-left-color:#3F97A6; padding-right: 5px;padding-left: 5px; }
-.ie7 .newt-post .newt-layout-cell {border:none !important; padding:0 !important; }
-.ie6 .newt-post .newt-layout-cell {border:none !important; padding:0 !important; }
-</style>
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <meta charset="utf-8">
 
-</head>
-<body>
-<div id="newt-main">
-    <div class="cleared reset-box"></div>
-    <div class="newt-header">
-        <div class="newt-header-position">
-            <div class="newt-header-wrapper">
-                <div class="cleared reset-box"></div>
-                <div class="newt-header-inner">
-                <div class="newt-headerobject"></div>
-                <script type="text/javascript" src="swfobject.js"></script>
-                <script type="text/javascript">
-                jQuery((function (swf) {
-                    return function () {
-                        swf.switchOffAutoHideShow();
-                        swf.registerObject("newt-flash-object", "9.0.0", "expressInstall.swf");
-                    }
-                })(swfobject));
-                </script>
-                <div id="newt-flash-area">
-                <div id="newt-flash-container">
-                <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="1881" height="470" id="newt-flash-object">
-                    <param name="movie" value="images/flash.swf" />
-                    <param name="quality" value="high" />
-                	<param name="scale" value="default" />
-                	<param name="wmode" value="transparent" />
-                	<param name="flashvars" value="color1=0xFFFFFF&amp;alpha1=.45&amp;framerate1=12&amp;loop=true&amp;wmode=transparent" />
-                    <param name="swfliveconnect" value="true" />
-                    <!--[if !IE]>-->
-                    <object type="application/x-shockwave-flash" data="images/flash.swf" width="1881" height="470">
-                        <param name="quality" value="high" />
-                	    <param name="scale" value="default" />
-                	    <param name="wmode" value="transparent" />
-                	    <param name="flashvars" value="color1=0xFFFFFF&amp;alpha1=.45&amp;framerate1=12&amp;loop=true&amp;wmode=transparent" />
-                        <param name="swfliveconnect" value="true" />
-                    <!--<![endif]-->
-                      	<div class="newt-flash-alt"><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></div>
-                    <!--[if !IE]>-->
-                    </object>
-                    <!--<![endif]-->
-                </object>
+          <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <main>
+            <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-collapsed">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.html">New Torque - Excelência em torque</a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="menu-collapsed">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active">
+                                <a href="index.php">Principal</a>
+                            </li>
+                            <li>
+                                <a href="a-empresa.php">A Empresa</a>
+                            </li>
+                            <li>
+                                <a href="servicos.php">Serviços</a>
+                            </li>
+                            <li>
+                                <a href="linha-equipamentos.php">Linha Equipamentos</a>
+                            </li>
+                            <li>
+                                <a href="clientes.php">Clientes</a>
+                            </li>
+                            <li>
+                                <a href="videos.php">Vídeos</a>
+                            </li>
+                            <li>
+                                <a href="localizacao.php">Localização</a>
+                            </li>
+                            <li>
+                                <a href="fale-conosco.php">Fale Conosco</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /.navbar-collapse -->
                 </div>
-                </div>
-                <div class="newt-logo">
-                                                </div>
-                </div>
-            </div>
-        </div>
-        
-    </div>
-    <div class="cleared reset-box"></div>
-<div class="newt-bar newt-nav">
-<div class="newt-nav-outer">
-<div class="newt-nav-wrapper">
-<div class="newt-nav-inner">
-	<div class="newt-nav-center">
-	<ul class="newt-hmenu">
-		<li>
-			<a href="./index.php">Principal</a>
-		</li>	
-		<li>
-			<a href="./a-empresa.php">A Empresa</a>
-		</li>	
-		<li>
-			<a href="./servicos.php">Serviços</a>
-		</li>	
-		<li>
-			<a href="./linha-equipamentos.php">Linha Equipamentos</a>
-		</li>	
-		<li>
-			<a href="./clientes.php">Clientes</a>
-		</li>	
-		<li>
-			<a href="./videos.php">V&iacute;deos</a>
-		</li>	
-		<li>
-			<a href="./localizacao.php">Localiza&ccedil;&atilde;o</a>
-		</li>	
-		<li>
-			<a href="./fale-conosco.php">Fale Conosco</a>
-		</li>	
-	</ul>
-	</div>
-</div>
-</div>
-</div>
-</div>
+                <!-- /.container -->
+            </nav>
 <div class="cleared reset-box"></div>
 <div class="newt-box newt-sheet">
         <div class="newt-box-body newt-sheet-body">
@@ -155,10 +109,10 @@
 <p>
 		<span style="color: rgb(201, 29, 32); font-size: 24px; font-weight: bold; line-height: 24px; -webkit-border-horizontal-spacing: 2px; -webkit-border-vertical-spacing: 2px;"></span>        
 <?
-		$id_query_serv = mysql_query("Select * from servicos where cd_servico = '{$_GET[cd]}' and bl_publicars ='1'");
+		$id_query_serv = mysql_query("Select * from servicos where cd_servico = '{$_GET['cd']}' and bl_publicars ='1'");
 		$arr_serv = mysql_fetch_assoc($id_query_serv);
 ?>
-        <h4><? echo($arr_serv[tit_servico])?></h4>
+        <h4><? echo($arr_serv['tit_servico'])?></h4>
 </p>
     	<div class="newt-layout-cell layout-item-2" style="width: 50%;">
         <table class="newt-article" border="0" cellspacing="0" cellpadding="0" style="width: 100%;">
@@ -168,7 +122,7 @@
                 </tr>
         
                 <tr>
-                    <td class="" style="padding-top: 8px; padding-right: 8px; padding-bottom: 8px; padding-left: 8px;"><h3><? echo($arr_serv[ds_servico]) ?></h3></td>
+                    <td class="" style="padding-top: 8px; padding-right: 8px; padding-bottom: 8px; padding-left: 8px;"><h3><? echo($arr_serv['ds_servico']) ?></h3></td>
                 </tr>
             </tbody>
         </table>
@@ -183,7 +137,7 @@
                 <tr>
                     <td class="" style="text-align: center; padding-top: 8px; padding-right: 8px; padding-bottom: 8px; padding-left: 8px;">
 <?
-				$id_query = mysql_query("Select * from fotos_servicos where cd_servico = '{$_GET[cd]}' and bl_publicarfs ='1' ORDER BY cd_ordemfs");
+				$id_query = mysql_query("Select * from fotos_servicos where cd_servico = '{$_GET['cd']}' and bl_publicarfs ='1' ORDER BY cd_ordemfs");
 				if (mysql_num_rows($id_query)){ 
 					$conta_fotos = 1;
 					while($arr = mysql_fetch_assoc($id_query)){
@@ -192,7 +146,7 @@
 				            echo("<br>");
 						}
 ?>
-                        <a href="fotos_servicos/foto_<? echo($arr[cd_foto_servico]) ?>.JPG" rel="lightbox[roadtrip]" title="<? echo($arr[ds_legenda]) ?>"><img src="fotos_servicos/mini_foto_<? echo($arr[cd_foto_servico]) ?>.JPG" title="<? echo($arr[ds_legenda]) ?>" border="1" class="thumb"></a>
+                        <a href="fotos_servicos/foto_<? echo($arr['cd_foto_servico']) ?>.JPG" rel="lightbox[roadtrip]" title="<? echo($arr['ds_legenda']) ?>"><img src="fotos_servicos/mini_foto_<? echo($arr['cd_foto_servico']) ?>.JPG" title="<? echo($arr['ds_legenda']) ?>" border="1" class="thumb"></a>
 <?
 						$conta_fotos += 1;
 					}
@@ -233,35 +187,14 @@
     		<div class="cleared"></div>
         </div>
     </div>
-    <div class="newt-footer">
-        <div class="newt-footer-body">
-            <div class="newt-footer-center">
-                <div class="newt-footer-wrapper">
-                    <div class="newt-footer-text">
-                        <a href="#" class="newt-rss-tag-icon" title="RSS"></a>
-                        <table class="newt-article" border="0" cellspacing="0" cellpadding="0" style="margin-right: auto; margin-left: auto; width: 100%;">
-    <tbody>
-        <tr>
-            <td style="text-align: center; width: 90%;" class="">
-                <h3>Rua Seis de Janeiro, 60 sala 15<br />
-                Centro Norte - Timóteo  Cep: 35180-030<br />
-                Email: contato@newtorque.com.br<br /></h3>
-            </td>
-
-            <td style="text-align: center; width: 10%;" class=""><a href="http://www.msideias.com.br" target="_blank" title="Soluções para suas ideias!"><img width="33" height="25" alt="" src="./images/logo_para_sites.png" style="margin-right: 1px; margin-left: 1px; cursor: text;" /></a><a href="adm" target="_blank" title="Acesso ao gerenciador de conteúdo do site"><img width="32" height="32" alt="" src="./images/gerenciador.png" style="margin-right: 1px; margin-left: 1px;" /></a></td>
-        </tr>
-    </tbody>
-</table>
-                        <div class="cleared"></div>
-                        <p class="newt-page-footer"></p>
-                    </div>
+            <footer class="footer">
+                <div class="container">
+                    <h4 class="text-muted text-center">Rua Cento e Trinta e Um nº 276<br/>
+                    Bairro Santa Maria - Timóteo Cep: 35180-142<br />
+                    Email: contato@newtorque.com.br<br /></h4>
                 </div>
-            </div>
-            <div class="cleared"></div>
-        </div>
-    </div>
-    <div class="cleared"></div>
+            </footer>
 </div>
-
-</body>
+        </main>
+    </body>
 </html>
